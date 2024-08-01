40e9adde1975b5a2d2bf45061cd968668e3ca235
Sithicha09 <sithicha.ban@vru.ac.th>
2024-08-01 16:03:17 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/week6/Home';
+import FlatListExample from './screens/week6/FlatListExample';
+
+export default function App() {
+  return (
+    <Home  />
+    // <FlatListExample  />
+  );
+}
+
